c8812aecf7c989da69db9caeb47b49ba5386e389
ElenaaAtanasoska <atanasoskaelena09@gmail.com>
2024-04-19 13:29:59 +0200

    first colloquium

---
+++ IntegratedSystems.Service/Interface/IPatientService.cs
@@ -0,0 +1,25 @@
+﻿using IntegratedSystems.Domain.Domain_Models;
+using IntegratedSystems.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedSystems.Service.Interface
+{
+    public interface IPatientService
+    {
+        public List<Patient> GetPatients();
+
+        public Patient GetPatientById(Guid? id);
+
+        public Patient CreateNewPatient(Patient patient);
+
+        public Patient UpdatePatient(Patient patient);
+
+        public Patient DeletePatient(Guid id);
+
+        void VaccinatePatient(VaccinationDTO model);
+    }
+}
